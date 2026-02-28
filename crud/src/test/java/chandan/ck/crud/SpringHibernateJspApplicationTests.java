db6b03a8e28b24de0c9ca4260b9b894d3184cbcf
Chandan-kr17 <chandan.singhck17@gmail.com>
2026-02-28 12:22:13 +0530

    Files added via upload

---
+++ crud/src/test/java/chandan/ck/crud/SpringHibernateJspApplicationTests.java
@@ -0,0 +1,13 @@
+package chandan.ck.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
